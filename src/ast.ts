32bb30661dbdc488eebf424c488bd2f031da97e2
fabiankachlock <fabian.kachlock@icloud.com>
2022-05-27 18:49:02 +0200

    feat: add effect type + node

--- src/ast.ts
+++ src/ast.ts
@@ -1,3 +1,37 @@
-export class CLIColorASTNode {}
+import { EffectsMap, SGREffect } from './types'
+
+export class CLIColorASTNode {
+    readonly effect: SGREffect
+
+    constructor(effect: SGREffect) {
+        this.effect = effect
+    }
+
+    clone(): CLIColorASTNode {
+        return new CLIColorASTNode({
+            weight: this.effect.weight,
+            italic: this.effect.italic,
+            underline: this.effect.underline,
+            foreground: this.effect.foreground,
+            background: this.effect.background,
+            blink: this.effect.blink,
+            crossedOut: this.effect.crossedOut,
+            concealed: this.effect.concealed,
+        })
+    }
+
+    Default(): CLIColorASTNode {
+        return new CLIColorASTNode({
+            weight: EffectsMap.weight.Default,
+            italic: EffectsMap.italic.Default,
+            underline: EffectsMap.underline.Default,
+            foreground: EffectsMap.foreground.Default,
+            background: EffectsMap.background.Default,
+            blink: EffectsMap.blink.Default,
+            crossedOut: EffectsMap.crossedOut.Default,
+            concealed: EffectsMap.concealed.Default,
+        })
+    }
+}
 
 export class CLIColorASTTree {}
--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,99 @@
+import { EFFECTS } from './effect'
+
 export type Token = {
-  token: string;
-  precedence: number;
-};
+    token: string
+    precedence: number
+}
+
+export type DefaultAble<T> = {
+    [key: string]: T
+    Default: T
+}
+
+export type PropertyOf<T> = T[keyof T]
+
+export const TextWeightEffect = {
+    [EFFECTS.NormalColorAndWeight]: 1,
+    [EFFECTS.Bold]: 2,
+    [EFFECTS.Faint]: 3,
+    Default: 1,
+}
+
+export const ItalicEffect = {
+    [EFFECTS.Italic]: true,
+    [EFFECTS.NotItalic]: false,
+    Default: false,
+}
+
+export const UnderlineEffect = {
+    [EFFECTS.NotUnderlined]: 1,
+    [EFFECTS.Underline]: 2,
+    [EFFECTS.DoublyUnderlined]: 3,
+    Default: 1,
+}
+
+export const NegativeEffect = {
+    [EFFECTS.NegativeImage]: true,
+    [EFFECTS.PositiveImage]: false,
+    Default: false,
+}
+
+export const ConcealedEffect = {
+    [EFFECTS.ConcealedCharacters]: true,
+    [EFFECTS.RevealedCharacters]: false,
+    Default: false,
+}
+
+export const CrossedOutEffect = {
+    [EFFECTS.CrossedOut]: true,
+    [EFFECTS.NotCrossedOut]: false,
+    Default: false,
+}
+
+export const BlinkEffect = {
+    [EFFECTS.Steady]: 1,
+    [EFFECTS.BlinkSlow]: 2,
+    [EFFECTS.BlinkRapid]: 3,
+    Default: 1,
+}
+
+export const ColorEffect = {
+    [EFFECTS.ColorDefault]: 1,
+    [EFFECTS.ColorBlack]: 2,
+    [EFFECTS.ColorRed]: 3,
+    [EFFECTS.ColorGreen]: 4,
+    [EFFECTS.ColorYellow]: 5,
+    [EFFECTS.ColorBlue]: 6,
+    [EFFECTS.ColorMagenta]: 7,
+    [EFFECTS.ColorCyan]: 8,
+    [EFFECTS.ColorWhite]: 9,
+    [EFFECTS.NormalColorAndWeight]: 1,
+    Default: 1,
+}
+
+export const EffectsMap: {
+    [K in keyof SGREffect]: DefaultAble<SGREffect[K]>
+} = {
+    weight: TextWeightEffect,
+    italic: ItalicEffect,
+    underline: UnderlineEffect,
+    foreground: ColorEffect,
+    background: ColorEffect,
+    blink: BlinkEffect,
+    crossedOut: CrossedOutEffect,
+    concealed: ConcealedEffect,
+}
+
+export type SGREffect = {
+    // font: '' not supported
+    weight: PropertyOf<typeof TextWeightEffect>
+    italic: PropertyOf<typeof ItalicEffect>
+    underline: PropertyOf<typeof UnderlineEffect>
+    foreground: PropertyOf<typeof ColorEffect>
+    background: PropertyOf<typeof ColorEffect>
+    blink: PropertyOf<typeof BlinkEffect>
+    crossedOut: PropertyOf<typeof CrossedOutEffect>
+    concealed: PropertyOf<typeof ConcealedEffect>
+}
+
+const t = <T>(x: DefaultAble<T>) => {}
