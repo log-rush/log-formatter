5d2b57c9bc036bd3bdd26ace532fc25dc65d0937
fabiankachlock <fabian.kachlock@icloud.com>
2022-05-28 15:22:31 +0200

    feat: add tests for commands

--- src/__test__/Commands.test.ts
+++ src/__test__/Commands.test.ts
@@ -5,7 +5,19 @@ import {
     SuccessCommandResult,
 } from '../commands'
 import { EFFECTS } from '../effect'
-import { BlinkEffect, DefaultSGREffects } from '../types'
+import {
+    BlinkEffect,
+    ColorEffect,
+    ColorModeEffect,
+    ConcealedEffect,
+    CrossedOutEffect,
+    DefaultSGREffects,
+    ItalicEffect,
+    NegativeEffect,
+    SGREffect,
+    TextWeightEffect,
+    UnderlineEffect,
+} from '../types'
 import { createSGREffects } from './testUtil'
 
 const expectMatch = (result: CommandResult): result is SuccessCommandResult => {
@@ -22,6 +34,34 @@ const expectKey = <T extends Record<string, V>, V = unknown>(
         expect(object[key]).toEqual(value)
     }
 }
+
+const createApplyCommandTestSuite =
+    (
+        command: typeof CommandParserMap[keyof typeof CommandParserMap],
+        expectedEffects: Partial<SGREffect>,
+    ) =>
+    () => {
+        it('should apply effects', () => {
+            const result = command('')
+            if (expectMatch(result)) {
+                for (const [key, value] of Object.entries(expectedEffects)) {
+                    expectKey(result.alteredEffects, key, value)
+                }
+            }
+        })
+
+        it('should should match', () => {
+            const result = command('')
+            expectMatch(result)
+        })
+
+        it('should return remaining command', () => {
+            const result = command('xxx')
+            if (expectMatch(result)) {
+                expect(result.remainingCommand).toBe('xxx')
+            }
+        })
+    }
 
 describe('Command Tests', () => {
     describe('Reset', () => {
@@ -46,4 +86,133 @@ describe('Command Tests', () => {
             }
         })
     })
+
+    describe(
+        'Bold',
+        createApplyCommandTestSuite(CommandParserMap[EFFECTS.Bold], {
+            weight: TextWeightEffect[EFFECTS.Bold],
+        }),
+    )
+
+    describe(
+        'Faint',
+        createApplyCommandTestSuite(CommandParserMap[EFFECTS.Faint], {
+            weight: TextWeightEffect[EFFECTS.Faint],
+        }),
+    )
+
+    describe(
+        'Italic',
+        createApplyCommandTestSuite(CommandParserMap[EFFECTS.Italic], {
+            italic: ItalicEffect[EFFECTS.Italic],
+        }),
+    )
+
+    describe(
+        'Underline',
+        createApplyCommandTestSuite(CommandParserMap[EFFECTS.Underline], {
+            underline: UnderlineEffect[EFFECTS.Underline],
+        }),
+    )
+
+    describe(
+        'BlinkSlow',
+        createApplyCommandTestSuite(CommandParserMap[EFFECTS.BlinkSlow], {
+            blink: BlinkEffect[EFFECTS.BlinkSlow],
+        }),
+    )
+
+    describe(
+        'BlinkRapid',
+        createApplyCommandTestSuite(CommandParserMap[EFFECTS.BlinkRapid], {
+            blink: BlinkEffect[EFFECTS.BlinkRapid],
+        }),
+    )
+
+    describe(
+        'NegativeImage',
+        createApplyCommandTestSuite(CommandParserMap[EFFECTS.NegativeImage], {
+            inverted: NegativeEffect[EFFECTS.NegativeImage],
+        }),
+    )
+
+    describe(
+        'ConcealedCharacters',
+        createApplyCommandTestSuite(
+            CommandParserMap[EFFECTS.ConcealedCharacters],
+            { concealed: ConcealedEffect[EFFECTS.ConcealedCharacters] },
+        ),
+    )
+
+    describe(
+        'CrossedOut',
+        createApplyCommandTestSuite(CommandParserMap[EFFECTS.CrossedOut], {
+            crossedOut: CrossedOutEffect[EFFECTS.CrossedOut],
+        }),
+    )
+
+    describe(
+        'DoublyUnderlined',
+        createApplyCommandTestSuite(
+            CommandParserMap[EFFECTS.DoublyUnderlined],
+            { underline: UnderlineEffect[EFFECTS.DoublyUnderlined] },
+        ),
+    )
+
+    describe(
+        'NormalColorAndWeight',
+        createApplyCommandTestSuite(
+            CommandParserMap[EFFECTS.NormalColorAndWeight],
+            {
+                weight: TextWeightEffect.Default,
+                foreground: ColorEffect.Default,
+                background: ColorEffect.Default,
+                foregroundMode: ColorModeEffect.Default,
+                backgroundMode: ColorModeEffect.Default,
+            },
+        ),
+    )
+
+    describe(
+        'NotItalic',
+        createApplyCommandTestSuite(CommandParserMap[EFFECTS.NotItalic], {
+            italic: ItalicEffect[EFFECTS.NotItalic],
+        }),
+    )
+
+    describe(
+        'NotUnderlined',
+        createApplyCommandTestSuite(CommandParserMap[EFFECTS.NotUnderlined], {
+            underline: UnderlineEffect[EFFECTS.NotUnderlined],
+        }),
+    )
+
+    describe(
+        'Steady',
+        createApplyCommandTestSuite(CommandParserMap[EFFECTS.Steady], {
+            blink: BlinkEffect[EFFECTS.Steady],
+        }),
+    )
+
+    describe(
+        'PositiveImage',
+        createApplyCommandTestSuite(CommandParserMap[EFFECTS.PositiveImage], {
+            inverted: NegativeEffect[EFFECTS.PositiveImage],
+        }),
+    )
+
+    describe(
+        'RevealedCharacters',
+        createApplyCommandTestSuite(
+            CommandParserMap[EFFECTS.RevealedCharacters],
+            { concealed: ConcealedEffect[EFFECTS.RevealedCharacters] },
+        ),
+    )
+
+    describe(
+        'NotCrossedOut',
+        createApplyCommandTestSuite(CommandParserMap[EFFECTS.NotCrossedOut], {
+            crossedOut: CrossedOutEffect[EFFECTS.NotCrossedOut],
+        }),
+    )
 })
