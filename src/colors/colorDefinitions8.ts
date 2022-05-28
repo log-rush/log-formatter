597e8a41be9243328f90d854cb5acf9abbf2fa89
fabiankachlock <fabian.kachlock@icloud.com>
2022-05-28 17:20:10 +0200

    feat: add color definitions for ColorMode8

---
+++ src/colors/colorDefinitions8.ts
@@ -0,0 +1,10 @@
+export const ColorMode8Colors = {
+    0: '#000000',
+    1: '#ff6d67',
+    2: '#59f68d',
+    3: '#f3f89d',
+    4: '#0cace3',
+    5: '#ff92d0',
+    6: '#99ecfd',
+    7: '#ffffff',
+}
